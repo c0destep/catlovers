4e1671a6d5861e252d9e79dfd805124255b01cf3
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2023-04-09 16:35:13 -0300

    feat: adicionado suporte há múltiplos idiomas
    Foram adicionados inicialmente o suporte há Português (Padrão) e Inglês. Também adicionado um botão
    para retorna ao top.
    
    re #12

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import Translator from "@andreasremdt/simple-translator"
+
+const portuguese = require("../languages/pt_BR.json")
+const english = require("../languages/en_US.json")
+
+const translator = new Translator({
+  defaultLanguage: 'pt_BR',
+  detectLanguage: true,
+  persist: true,
+  persistKey: 'preferred_language'
+});
+
+translator.add('pt_BR', portuguese).add('en_US', english).translatePageTo('pt_BR')
+
+window.onscroll = () => {
+  backToTop()
+}
+
+// TODO Fazer um seletor de idiomas
+
+/*for (let button of document.querySelectorAll('button')) {
+  button.addEventListener('click', (evt) => {
+    translator.translatePageTo(evt.target.dataset.lang)
+  })
+}*/
+
+function backToTop() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    document.getElementById("backTop").classList.add('page-top__visible');
+  } else {
+    document.getElementById("backTop").classList.remove('page-top__visible');
+  }
+}
