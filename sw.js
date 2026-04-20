06e7e4b7c526ef8555f66c2972a59c984f423941
Lucas Alves <lucas.alves.capristrano59@gmail.com>
2026-04-20 16:35:25 -0300

    fix: use dynamic caching strategy in service worker for Parcel compatibility

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-const CACHE_NAME = 'catlovers-v1';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'catlovers-v2';
+
+// Cacheamos apenas as páginas principais que mantêm o nome fixo
+const PRE_CACHE = [
   './',
   './index.html',
   './about.html',
@@ -10,24 +12,48 @@ const ASSETS_TO_CACHE = [
   './quiz.html',
   './post.html',
   './happy-endings.html',
-  './css/main.css',
-  './js/main.js',
-  './cats.json',
-  './img/catlovers.jpg',
-  './favicon-32.png',
   './site.webmanifest'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then((cache) => cache.addAll(PRE_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((response) => {
+        // Salva dinamicamente no cache para uso futuro
+        if (response && response.status === 200 && response.type === 'basic') {
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return response;
+      });
+    })
   );
 });
